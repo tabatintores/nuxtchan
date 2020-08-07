8863ac775278c871fd8692c9ba132c5950c5d22d
Kanat Kolbekov <k.kolbekov@thecoders.ru>
2020-08-07 22:26:40 +0500

    Доработка сайдбара

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,8 +1,9 @@
 <template>
     <section class="sidebar">
         <div class="sidebar__controls">
-            <svg @click="updateBoard" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="redo" class="sidebar__controls-update svg-inline--fa fa-redo fa-w-16" role="img" viewBox="0 0 512 512"><path fill="currentColor" d="M500.33 0h-47.41a12 12 0 0 0-12 12.57l4 82.76A247.42 247.42 0 0 0 256 8C119.34 8 7.9 119.53 8 256.19 8.1 393.07 119.1 504 256 504a247.1 247.1 0 0 0 166.18-63.91 12 12 0 0 0 .48-17.43l-34-34a12 12 0 0 0-16.38-.55A176 176 0 1 1 402.1 157.8l-101.53-4.87a12 12 0 0 0-12.57 12v47.41a12 12 0 0 0 12 12h200.33a12 12 0 0 0 12-12V12a12 12 0 0 0-12-12z"/></svg>
-<!--            <a href="javascript:void(0)" @click="updateBoard">Обновить</a>-->
+            <nuxt-link to="/" tag="i" class="sidebar__icon sidebar__icon--home" exact/>
+            <i class="sidebar__icon sidebar__icon--refresh" @click="updateBoard"></i>
+            <!--            <a href="javascript:void(0)" @click="updateBoard">Обновить</a>-->
         </div>
         <ul class="sidebar__categories">
             <li class="sidebar__category" v-for="(category, boards) of Object.entries(boardsList)" :key="category[0]">
@@ -64,6 +65,7 @@
 
 <style lang="scss" scoped>
     @import "@/assets/scss/style";
+    $icon-size: 20px;
     .sidebar {
         position: fixed;
         height: 100%;
@@ -74,8 +76,26 @@
         padding: 10px;
         overflow-x: hidden;
         user-select: none;
+        &__icon {
+            cursor: pointer;
+            transition: all .25s ease-in-out;
+            &--home {
+                @include icon($lightning-icon, 24px);
+            }
+            &--refresh {
+                @include icon($refresh-icon, $icon-size);
+            }
+            &:hover {
+                transform: scale(1.2);
+            }
+            &:nth-child(n+2) {
+                margin-left: 5px;
+            }
+        }
         &__controls {
             padding-bottom: 10px;
+            display: flex;
+            align-items: center;
             &-update {
                 height: 20px;
                 cursor: pointer;
